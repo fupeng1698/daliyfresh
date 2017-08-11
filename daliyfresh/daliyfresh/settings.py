8562ddf471e1fcba97d74af0bc31baa650aeed73
fupeng.it <602516169@qq.com>
2017-08-11 15:01:54 +0800

    添加静态文件

--- daliyfresh/daliyfresh/settings.py
+++ daliyfresh/daliyfresh/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'daliyfresh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE':'django.db.backends.mysql',
+        'HOST':'localhost',
+        'PORT':'3306',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'NAME':'test2'
     }
 }
 
@@ -120,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
